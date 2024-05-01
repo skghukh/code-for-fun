01c39e64813e2e6e4d25fb06abd1355ec7fadde0
verma-shailendra <shailendra.verma@groww.in>
2024-05-01 18:37:50 +0530

    IntegerToRoman

--- LeetCodeProblems/IntegerToRoman.py
+++ LeetCodeProblems/IntegerToRoman.py
@@ -7,51 +7,6 @@ class Solution:
             while num >= i[0]:
                 num = num - i[0]
                 result = result + i[1]
-        print("first is " + result)
-
-        result = ""
-        num = num2
-        while num >= 1000:
-            result = result + 'M'
-            num = num - 1000
-        while num >= 900:
-            result = result + 'CM'
-            num = num - 900
-        while num >= 500:
-            result = result + 'D'
-            num = num - 500
-        while num >= 400:
-            result = result + 'CD'
-            num = num - 400
-        while num >= 100:
-            result = result + 'C'
-            num = num - 100
-        while num >= 90:
-            result = result + 'XC'
-            num = num - 90
-        while num >= 50:
-            result = result + 'L'
-            num = num - 50
-        while num >= 40:
-            result = result + 'XL'
-            num = num - 40
-        while num >= 10:
-            result = result + 'X'
-            num = num - 10
-        while num >= 9:
-            result = result + 'IX'
-            num = num - 9
-        while num >= 5:
-            result = result + 'V'
-            num = num - 5
-        while num >= 4:
-            result = result + 'IV'
-            num = num - 4
-        while num >= 1:
-            result = result + 'I'
-            num = num - 1;
-
-        print("Second is " + result)
         return result
 
 def main():
@@ -60,3 +15,111 @@ def main():
 
 if __name__ == '__main__':
      main()
+
+
+#https://leetcode.com/problems/integer-to-roman/
+# Roman
+# numerals
+# are
+# represented
+# by
+# seven
+# different
+# symbols: I, V, X, L, C, D and M.
+#
+# Symbol
+# Value
+# I
+# 1
+# V
+# 5
+# X
+# 10
+# L
+# 50
+# C
+# 100
+# D
+# 500
+# M
+# 1000
+# For
+# example, 2 is written as II in Roman
+# numeral, just
+# two
+# one
+# 's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+#
+# Roman
+# numerals
+# are
+# usually
+# written
+# largest
+# to
+# smallest
+# from left to
+#
+# right.However, the
+# numeral
+# for four is not IIII.Instead, the number four is written as IV.Because the one is before the five we subtract it making four.The same principle applies to the number nine, which is written as IX.There are six instances where subtraction is used:
+#
+# I
+# can
+# be
+# placed
+# before
+# V(5) and X(10)
+# to
+# make
+# 4 and 9.
+# X
+# can
+# be
+# placed
+# before
+# L(50) and C(100)
+# to
+# make
+# 40 and 90.
+# C
+# can
+# be
+# placed
+# before
+# D(500) and M(1000)
+# to
+# make
+# 400 and 900.
+# Given
+# an
+# integer, convert
+# it
+# to
+# a
+# roman
+# numeral.
+#
+# Example
+# 1:
+#
+# Input: num = 3
+# Output: "III"
+# Explanation: 3 is represented as 3
+# ones.
+# Example
+# 2:
+#
+# Input: num = 58
+# Output: "LVIII"
+# Explanation: L = 50, V = 5, III = 3.
+# Example
+# 3:
+#
+# Input: num = 1994
+# Output: "MCMXCIV"
+# Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+#
+# Constraints:
+#
+# 1 <= num <= 3999
